8b0580f9a50f0df46691198eb5fa678bb125c92d
sebastianto <sebastt.93@gmail.com>
2021-08-02 17:14:15 +1000

    1.ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
+import React from "react";
+import ReactDOM from "react-dom";
 
-// const container = document.getElementById('app');
+const element = <h1>Hello </h1>
+const container = document.getElementById('app')
 
-// container.appendChild(element);
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const element = <h1>Hello, Platzi Badges!</h1>;
-
-const container = document.getElementById('app');
-
-// ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(element, container)

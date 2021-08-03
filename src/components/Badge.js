947ca8a4c5cc611f331fb9ea5545c9f751546c0c
sebastianto <sebastt.93@gmail.com>
2021-08-03 10:21:22 +1000

    2.FirstComponent

---
+++ src/components/Badge.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import confLogo from '../images/badge-header.svg'
+
+class Badge extends React.Component {
+  render() {
+    return (
+      <div>
+
+        <div>
+          <img src={confLogo} alt="logo" />
+        </div>
+
+        <div>
+          <img src={`https://www.gravatar.com/avatar?d=identicon`} alt="Avatar" />
+          <h1>
+            Richard <br /> Kaufman
+          </h1>
+        </div>
+
+        <div>
+          <p>Frontend Engineer</p>
+          <p>@sparragus</p>
+        </div>
+
+        <div>#Platziconf</div>
+      </div>
+    );
+  }
+}
+
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = <h1>Hello </h1>
-const container = document.getElementById('app')
+import Badge from "./components/Badge";
 
+const container = document.getElementById("app");
 
-ReactDOM.render(element, container)
+ReactDOM.render(<Badge />, container);

0b0994c911183f3f427bc6ca8e79c8f9701901c1
sebastianto <sebastt.93@gmail.com>
2021-08-03 11:00:03 +1000

    4.Props

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -16,15 +16,15 @@ class Badge extends React.Component {
         </div>
 
         <div className="badge_section-name">
-          <img className="badge_avatar" src={`https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon`} alt="Avatar" />
+          <img className="badge_avatar" src={this.props.avatarUrl} alt="Avatar" />
           <h1>
-            Richard <br /> Kaufman
+            {this.props.firstName} <br /> {this.props.lastName}
           </h1>
         </div>
 
         <div className="badge_section-info">
-          <h3>Frontend Engineer</h3>
-          <div>@sparragus</div>
+          <h3>{this.props.jobTitle}</h3>
+          <div>@{this.props.twitter}</div>
         </div>
 
         <div className="badge_footer ">#Platziconf</div>
--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //Bootstrap
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css";
 //styles
-import './global.css'
+import "./global.css";
 
 //componentes
 import Badge from "./components/Badge";
 
-
 const container = document.getElementById("app");
 
-ReactDOM.render(<Badge />, container);
+ReactDOM.render(
+  <Badge
+    firstName="Lilly"
+    lastName="Kaufman"
+    avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+    jobTitle="Senior Dev"
+    twitter="Sparragus"
+  />,
+  container
+);

dccba40b597ad25d61e439574bb1505f57cfb22f
sebastianto <sebastt.93@gmail.com>
2021-08-03 10:38:17 +1000

    3.AddingStyles

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,29 +1,33 @@
 import React from "react";
 
+//Styles
+import './styles/Badge.css'
+//Images
 import confLogo from '../images/badge-header.svg'
+
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
+      <div className="badge">
 
-        <div>
+        <div className="badge_header">
           <img src={confLogo} alt="logo" />
         </div>
 
-        <div>
-          <img src={`https://www.gravatar.com/avatar?d=identicon`} alt="Avatar" />
+        <div className="badge_section-name">
+          <img className="badge_avatar" src={`https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon`} alt="Avatar" />
           <h1>
             Richard <br /> Kaufman
           </h1>
         </div>
 
-        <div>
-          <p>Frontend Engineer</p>
-          <p>@sparragus</p>
+        <div className="badge_section-info">
+          <h3>Frontend Engineer</h3>
+          <div>@sparragus</div>
         </div>
 
-        <div>#Platziconf</div>
+        <div className="badge_footer ">#Platziconf</div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+//styles
+import './global.css'
+
+//componentes
 import Badge from "./components/Badge";
+
 
 const container = document.getElementById("app");
 

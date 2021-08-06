100787aa09453c16df9b065fd7346ffee1b0cf44
sebastianto <sebastt.93@gmail.com>
2021-08-06 11:42:16 +1000

    21.ComponentsAndContainers(Split)

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import Layout from "./Layout";
 import Home from "../pages/Home";
 import Badges from "../pages/Badges";
 import BadgeNew from "../pages/BadgeNew";
-import BadgeDetails from "../pages/BadgeDetails";
+import BadgeDetailsContainer from "../pages/BadgeDetailsContainer";
 import BadgeEdit from "../pages/BadgeEdit";
 import NotFound from "../pages/NotFound";
 
@@ -18,7 +18,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
-          <Route exact path="/badges/:badgeId" component={BadgeDetails} />
+          <Route exact path="/badges/:badgeId" component={BadgeDetailsContainer} />
           <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
           <Route component={NotFound} />
         </Switch>

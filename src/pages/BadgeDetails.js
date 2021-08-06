4ebe66ecc0e6419d2276e2535453d9d283274ea5
sebastianto <sebastt.93@gmail.com>
2021-08-06 13:09:19 +1000

    22.IntroReactDOM.createPortal()

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM  from "react-dom";
 import { Link } from "react-router-dom";
 
 
@@ -51,6 +52,7 @@ function BadgeDetails(props) {
               </div>
               <div>
                 <button className="btn btn-danger">Delete</button>
+                { ReactDOM.createPortal(<h1>No stoy aca</h1>, document.getElementById("modal")) }
               </div>
             </div>
           </div>
